24d0c8323e2d1920651a8ee70dab23d46cdcfadb
4chaz <4chaz@DESKTOP-KPJR5NU>
2019-09-29 16:38:00 -0500

    Paint job estimator final

--- PaintPrimeCounter/src/main/java/PaintJobEstimator.java
+++ PaintPrimeCounter/src/main/java/PaintJobEstimator.java
@@ -2,20 +2,83 @@
 import java.util.Scanner;
 
 public class PaintJobEstimator {
+
     public static void main(String[] args) {
+        final double CHARGE_PER_HOUR = 18.00;
+        int numberOfRooms = rooms();
+        double totalWallSpace = getTotalWallSpace(numberOfRooms);
+        double priceOfPaint = getPaintPrice();
+        double gallonsOfPaint = calculateGallonsOfPaint(totalWallSpace);
+        double hoursOfLabor = calculateHoursOfLabor(totalWallSpace);
+        double costOfPaint = calculateCostOfPaint(priceOfPaint, gallonsOfPaint);
+        double laborCharges = cacluateLaborCharges(hoursOfLabor, CHARGE_PER_HOUR);
+        double totalCost = totalCost(laborCharges, costOfPaint);
+        displayData(gallonsOfPaint, costOfPaint, hoursOfLabor,laborCharges,totalCost);
         
-        
-        
+    }
+
+    public static int rooms() {
+        int numberOfRooms;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("How many rooms are being painted?");
+        numberOfRooms = keyboard.nextInt();
+        return numberOfRooms;
+    }
+
+    public static double getPaintPrice() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is the price per gallon of paint?");
+        double priceOfPaint;
+        priceOfPaint = keyboard.nextDouble();
+        return priceOfPaint;
+    }
+
+    public static double getTotalWallSpace(int numberOfRooms) {
+        Scanner keyboard = new Scanner(System.in);
+        double squareFeet;
+        double totalSquareFeet = 0;
+        for (int room = 1; room <= numberOfRooms; room++) {
+            System.out.println("Please enter the Square feet of wall space in room " + room);
+            squareFeet = keyboard.nextDouble();
+            totalSquareFeet += squareFeet;
+        }
+        return totalSquareFeet;
+    }
+
+    public static double calculateGallonsOfPaint(double totalWallSpace) {
+        double gallonsOfPaint = (totalWallSpace / 115);
+        return gallonsOfPaint;
+    }
+
+    public static double calculateHoursOfLabor(double totalWallSpace) {
+        double hoursOfLabor = (totalWallSpace / 115) * 8;
+        return hoursOfLabor;
+    }
+
+    public static double calculateCostOfPaint(double priceOfPaint, double caculateGallonsOfPaint) {
+        double costOfPaint = caculateGallonsOfPaint * priceOfPaint;
+        return costOfPaint;
+    }
+
+    public static double cacluateLaborCharges(double calculateHoursOfLabor, double chargePerHour) {
+        double laborCharges = calculateHoursOfLabor * chargePerHour;
+        return laborCharges;
 
     }
-    public static void roomCounter(){
-        System.out.println ("How many rooms are being painted?");
-        Scanner keyboard = new Scanner(System.in);
-        int rooms = keyboard.nextInt();
-        
-        
-        
+
+    public static double totalCost(double laborCharges, double costOfPaint) {
+        double totalCost = laborCharges + costOfPaint;
+        return totalCost;
+
     }
-    
+
+    public static void displayData(double gallonsOfPaint, double costOfPaint,
+            double hoursOfLabor, double laborCharges, double totalCost) {
+        System.out.println(" The number of gallons of paint Required: " + gallonsOfPaint
+                + "\n The hours of labor required: " + hoursOfLabor + "\n The cost of paint: "
+                + costOfPaint + "\n The labor Charges: " + laborCharges
+                + "\n The total cost of the paint job: " + totalCost);
+
+    }
 
 }
