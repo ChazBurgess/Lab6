baa7eba015ad87326b227e5a4455b7a230ad08eb
4chaz <4chaz@DESKTOP-KPJR5NU>
2019-09-29 12:05:03 -0500

    initial commit

---
+++ PaintPrimeCounter/src/main/java/PaintJobEstimator.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class PaintJobEstimator {
+    public static void main(String[] args) {
+        
+        
+        
+
+    }
+    public static void roomCounter(){
+        System.out.println ("How many rooms are being painted?");
+        Scanner keyboard = new Scanner(System.in);
+        int rooms = keyboard.nextInt();
+        
+        
+        
+    }
+    
+
+}
