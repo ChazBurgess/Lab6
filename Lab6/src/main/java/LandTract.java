18ecb0e146a61cd90d99b7d14b5ba6c849947f5d
4chaz <4chaz@DESKTOP-KPJR5NU>
2019-11-03 10:42:33 -0600

    initial commit Land tract

---
+++ Lab6/src/main/java/LandTract.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class LandTract {
+    public static void main(String[] args) {
+   
+    double length = 0;
+    double width = 0;
+    double area1 = 0;
+    double length2 = 0;
+    double width2 = 0;
+    double area2=0;
+    
+    Scanner keyboard = new Scanner(System.in);
+    
+    System.out.print ("Enter the length of the first tract of land: ");
+    length = keyboard.nextDouble();
+    System.out.print ("Enter the width of the first tract of land: ");
+    width = keyboard.nextDouble();
+    
+    System.out.print ("Enter the length of the second tract of land: ");
+    length2 = keyboard.nextDouble();
+    System.out.print ("Enter the width of the second tract of land: ");
+    width2 = keyboard.nextDouble();
+    
+    
+          
+          
+  }
+}
